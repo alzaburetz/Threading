38b4f89a7cadbbcae2c549cc33094de542563fb2
alzaburetz <anton.motin.dev@gmail.com>
2021-04-25 16:36:30 +0300

    Cancellation token added

--- Threading/Task1/MyEventHandler.cs
+++ Threading/Task1/MyEventHandler.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading;
 
 namespace Threading
 {
@@ -11,7 +10,15 @@ namespace Threading
         {
             _action = action;
         }
+        public MyEventHandler(Action action, CancellationToken token)
+        {
+            _action = action;
+            _token = token;
+        }
         public void InvokeAction() => _action.Invoke();
+        public bool HasCancellactionToken() => _token == CancellationToken.None;
+        public CancellationToken Token => _token;
         private readonly Action _action;
+        private readonly CancellationToken _token;
     }
 }
--- Threading/Task1/MyThreadPool.cs
+++ Threading/Task1/MyThreadPool.cs
@@ -16,6 +16,13 @@ namespace Threading
             _actions.Enqueue(handler);
             return handler;
         }
+        public MyEventHandler QueueUserWorkItem(Action action, CancellationToken token)
+        {
+            action = action ?? throw new ArgumentNullException(nameof(action));
+            var handler = new MyEventHandler(action, token);
+            _actions.Enqueue(handler);
+            return handler;
+        }
         public MyThreadPool()
         {
             _actions = new Queue<MyEventHandler>();
@@ -29,6 +36,10 @@ namespace Threading
             {
                 if (_actions.TryDequeue(out var result))
                 {
+                    if (result.HasCancellactionToken() && result.Token.IsCancellationRequested)
+                    {
+                        continue;
+                    }
                     result.InvokeAction();
                     result.OnFinished?.Invoke(this, EventArgs.Empty);
                 }
