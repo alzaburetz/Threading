93a2e6c125f4e2af79332e6cd34a2adebbd9d159
alzaburetz <anton.motin.dev@gmail.com>
2021-06-10 10:22:24 +0300

    WhenAllOrError method added

--- Threading/Helper.cs
+++ Threading/Helper.cs
@@ -10,17 +10,46 @@ namespace Threading
     {
         public static async Task<TResult> WithCancellation<TResult>(this Task<TResult> task, CancellationToken ct)
         {
-            var tcs = new TaskCompletionSource<TResult>(TaskCreationOptions.RunContinuationsAsynchronously);
-            ct.Register(() => tcs.TrySetCanceled(), useSynchronizationContext: false);
-            var cancellableTask = await Task.WhenAny(task, tcs.Task);
+            if (task == null)
+                throw new ArgumentNullException(nameof(task));
 
-            if (cancellableTask == tcs.Task)
+            while (!task.IsCompleted)
             {
-                task.ContinueWith((result) => task.Exception.Flatten(),
-                    TaskContinuationOptions.OnlyOnFaulted |
-                    TaskContinuationOptions.ExecuteSynchronously);
+                await Task.Delay(30);
+                ct.ThrowIfCancellationRequested();
             }
-            return cancellableTask.Result;
+
+            return await task;
+        }
+
+        public static Task<TResult[]> WhenAllOrError<TResult>(params Task<TResult>[] tasks)
+        {
+            return WhenAllOrErrorImplementation(tasks);
+        }
+
+        static Task<TResult[]> WhenAllOrErrorImplementation<TResult>(Task<TResult>[] tasks, CancellationToken token = default(CancellationToken))
+        {
+            var tcs = new TaskCompletionSource<TResult[]>();
+            var result = new TResult[tasks.Length];
+            var pending = result.Length;
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                var ti = i;
+                tasks[i].ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        tcs.TrySetException();
+                    }
+                    result[ti] = t.Result;
+                    if (0 == Interlocked.Decrement(ref pending))
+                        tcs.SetResult(result);
+                }, CancellationToken.None,
+                TaskContinuationOptions.ExecuteSynchronously,
+                TaskScheduler.Default);
+            }
+
+            return tcs.Task;
         }
     }
 }
